76ff13f4638a9f78a8f9ab0f005a946b4d35c7be
Ajal Jose <ajaljosem@gmail.com>
2021-12-31 13:45:41 +0530

    webby first page created with js

---
+++ scripts/first.js
@@ -0,0 +1,30 @@
+var dar;
+$(document).ready(function(){
+    dar=0;
+    $('body').css('transition-duration','2s');
+  });
+  $("#mood").click(function(){
+      
+       if(dar==0)
+           dark();
+       else
+           light();
+    
+        
+    });
+
+function dark(){
+    $("body").css("background-color","#14213d");
+    $("h1").css("color","#fefae0");
+    //debugger;
+    $('#mood').html('Light');
+    dar=1;
+
+}
+
+function light(){
+    $("body").css("background-color","#fefae0");
+    $("h1").css("color","#14213d");
+    dar=0;
+    $('#mood').html('Dark');
+}
