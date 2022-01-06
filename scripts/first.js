89dd82ee5a54301acee349d90676ba2c03bf7f04
Ajal Jose <ajaljosem@gmail.com>
2022-01-06 09:38:23 +0530

    nav bar added and background for the p tag added

--- scripts/first.js
+++ scripts/first.js
@@ -28,3 +28,16 @@ function light(){
     dar=0;
     $('#mood').html('Dark');
 }
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".navbar", {
+  scrollTrigger: {
+    trigger: ".two",
+    endTrigger: ".one",
+    scrub: true,
+    toggleActions: "restart none none none",
+    start: "bottom bottom"
+  },
+  snap: 0,
+  backgroundColor: "black"
+});
